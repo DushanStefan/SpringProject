e0a2454179b07a555ca1bd7420b74e29d44a606b
Dushan Fernando <dushanfernando850@gmail.com>
2025-03-24 01:42:08 +0530

    'JWT'

---
+++ 14.Generating JWT Token/src/main/java/com/dushan/springsecex/service/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.dushan.springsecex.service;
+
+import com.dushan.springsecex.model.UserPrincipal;
+import com.dushan.springsecex.model.Users;
+import com.dushan.springsecex.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepo.findByUsername(username);
+        if (user == null) {
+            System.out.println("User Not Found");
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
