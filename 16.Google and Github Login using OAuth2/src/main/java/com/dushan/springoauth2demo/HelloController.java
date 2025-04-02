d17b7979c16cf152da8a931ca81c762c07959d64
Dushan Fernando <dushanfernando850@gmail.com>
2025-04-03 00:15:53 +0530

    Google and Github Login using OAuth2

---
+++ 16.Google and Github Login using OAuth2/src/main/java/com/dushan/springoauth2demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.dushan.springoauth2demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet() {
+        return "Welcome to dushan, dushan.com";
+    }
+}
